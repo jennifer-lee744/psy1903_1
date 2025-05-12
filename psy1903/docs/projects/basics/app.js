33dd1d77fc27e1226125d7d3f03cdb8379f84c19
Jenny Lee <jennifer.lee744@du.edu>
2025-05-12 11:43:06 -0600

    learning to use variabes

--- psy1903/docs/projects/basics/app.js
+++ psy1903/docs/projects/basics/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+let experiment = 'Stroop';
+let trialCount = 20;
+let colors = ['red', 'green', 'blue'];
+
+alert('Welcome to the ' + experiment + ' experiment!');
+
+trialCountMax = 40;
+
+let halfWayCount = trialCountMax / 2;
+
+console.log(halfWayCount);
