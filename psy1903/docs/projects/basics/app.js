4a6e74f9b2ebe3e0e70e2c796325f08eb5614554
Jenny Lee <jennifer.lee744@du.edu>
2025-05-12 19:03:42 -0600

    data type

--- psy1903/docs/projects/basics/app.js
+++ psy1903/docs/projects/basics/app.js
@@ -1,11 +1,34 @@
-let experiment = 'Stroop';
-let trialCount = 20;
+// Define a variable to hold experiment name
+
+let experiment = 'Stroop'; //string data type
+
+let welcomeMessage = `
+Welcome to our` + experiment + ` experiment!
+Please read the instructions carefully`;
+
+console.log(welcomeMessage)
+console.log(typeof experiment);
+console.log(experiment.charAt(5)); //gives us the 5th letter
+let trialCount = 20; //number data type
+console.log(typeof trialCount);
+//TODO: Randomize colors
 let colors = ['red', 'green', 'blue'];
 
 alert('Welcome to the ' + experiment + ' experiment!');
 
 trialCountMax = 40;
-
+// A the halfway point we will display a pause screen
 let halfWayCount = trialCountMax / 2;
 
-console.log(halfWayCount);
+console.log(halfWayCount); //Expected: 20
+
+/*
+can span multiple lines if use the astrisk
+*/
+
+//boolean datatype
+let correct = true;
+console.log(typeof correct);
+
+console.log(10 > 15); //expect false
+
